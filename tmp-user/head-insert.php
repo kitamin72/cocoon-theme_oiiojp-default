0022389c71d8d62d3f0c561eb321a3e3c1b472dd
kitamin <ryuji@oiio.jp>
2019-02-28 00:35:11 +0900

    エディタと公開本文との書式同期作業完了

--- tmp-user/head-insert.php
+++ tmp-user/head-insert.php
@@ -9,3 +9,16 @@
 
 <?php endif; ?>
 <?php //ログインユーザーも含めてカウントする場合は以下に挿入 ?>
+
+<link href="https://fonts.googleapis.com/css?family=Kosugi+Maru" rel="stylesheet">
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'kqb0pze',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+
