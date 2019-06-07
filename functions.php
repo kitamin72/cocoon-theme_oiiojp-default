95e3ef32b56178e0c0de5083b2f9b9da8d4139cf
kitamin <ryuji@oiio.jp>
2019-06-07 19:50:35 +0900

    ヘッダーエリアに宣伝を出すカスタマイズ

--- functions.php
+++ functions.php
@@ -56,3 +56,23 @@ function set_popular_postargs($args) {
 
     return $args;
 }
+
+////////////////////////////////////////////////////////////
+// ヘッダー領域にアナウンスを出すカスタマイズ
+////////////////////////////////////////////////////////////
+// アナウンス枠エリアウィジェット
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+            'name' => 'ヘッダー領域追加コンテンツ',
+            'id' => 'add-header-contents',
+            'description' => 'ヘッダー領域に追加コンテンツを表示するウィジェットです。',
+            'before_widget' => '<div class="add-header-contents">',
+            'after_widget' => '</div>',
+    ));
+}
+// ヘッダーロゴの下に出力をする
+add_filter('wp_header_logo_after_open', 'add_header_contents');
+function add_header_contents() {
+    dynamic_sidebar('add-header-contents');
+}
+
